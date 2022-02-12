4e44b7b86e9060891e7be3b220a23ff47b6e1ef7
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-12 22:18:29 +0100

    card search

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import "./usercard.css";
 function UserCard({ name, avatarUrl, id }) {
   return (
-    <div key={id}>
-      <img width={70} src={avatarUrl} />
-      <h2>{name}</h2>
-      <p>Visit Profile</p>
+    <div className="userCard" key={id}>
+      <img height={70} width={70} src={avatarUrl} />
+      <div>
+        <h2>{name}</h2>
+        <p>Visit Profile</p>
+      </div>
     </div>
   );
 }
