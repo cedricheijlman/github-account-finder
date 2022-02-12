55ef2633a2a5d6957d3945ee01c1c3d76ca1de8b
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-12 21:39:10 +0100

    search users

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Axios from "axios";
 function Home() {
+  const [search, setSearch] = useState(null);
+
+  const handleSearch = async () => {
+    await Axios.get(
+      `${process.env.REACT_APP_GITHUB_URL}/search/users?q=${encodeURIComponent(
+        "cedric heijlman"
+      )}`,
+      {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    ).then((result) => {});
+  };
+
   return (
     <div className="homePage">
       <h1>Search for an Github account</h1>
-      <div>
-        <input placeholder="Search" />
-        <button>Go</button>
+      <div className="homePage__search">
+        <input
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          placeholder="Search"
+        />
+        <button onClick={handleSearch}>Go</button>
       </div>
+      <div className="homePage__searchResults"></div>
     </div>
   );
 }
